a14838b614bd2bfe9a2079cfe2783c43a9f3dc2a
Max Semenik <maxsem.wiki@gmail.com>
2016-12-06 16:27:28 -0800

    Add another schema version
    Just keeps existing data flowing, new parameter will be taken into
    account as part of T152559.
    
    Bug: T152513
    
    Change-Id: Iff4cab884cc2a318e3f521ba3ba45e926a568881
    Depends-On: Ieb757994f91d4f2760dc4919c322abca5200fc0a

--- interactive/events-all.sql
+++ interactive/events-all.sql
@@ -8,4 +8,18 @@ SELECT
         timestamp >= '{from_timestamp}'
         AND timestamp < '{to_timestamp}'
     GROUP BY event_action, event_feature
+
+UNION ALL
+
+SELECT
+        DATE(timestamp) AS day,
+        event_action AS action,
+        event_feature AS feature,
+        SUM(event_sampling) AS events
+    FROM Kartographer_16132745
+    WHERE
+        timestamp >= '{from_timestamp}'
+        AND timestamp < '{to_timestamp}'
+    GROUP BY event_action, event_feature
+
 ;
--- interactive/events.sql
+++ interactive/events.sql
@@ -16,4 +16,25 @@ SELECT
         AND timestamp >= '{from_timestamp}'
         AND timestamp < '{to_timestamp}'
     GROUP BY event_action, event_feature
+
+UNION ALL
+
+SELECT
+        DATE(timestamp) AS day,
+        event_action AS action,
+        event_feature AS feature,
+        SUM(event_sampling) AS events
+    FROM Kartographer_16132745, metawiki.sites
+    WHERE
+        wiki = site_global_key
+        AND (
+            '{family}' = 'all'
+            -- Non-special wikis
+            OR site_group = '{family}'
+            -- site_group for 'metawiki' is 'meta', etc
+            OR ( '{family}' = 'special' AND INSTR(wiki, site_group) = 0 )
+        )
+        AND timestamp >= '{from_timestamp}'
+        AND timestamp < '{to_timestamp}'
+    GROUP BY event_action, event_feature
 ;
