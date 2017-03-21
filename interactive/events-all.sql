3122652ccd75474c721c0e0c67cd9d5bd4e67a97
Nuria Ruiz <nuria@wikimedia.org>
2017-03-21 15:49:13 -0700

    Adding renamed tables to sql union statements
    Code will be merged when rename is finished.
    
    Please see:
    https://phabricator.wikimedia.org/T160454
    
    Bug: T160454
    Change-Id: I4fd36f394f011f38c38230607ea6e936fa5c3e91

--- interactive/events-all.sql
+++ interactive/events-all.sql
@@ -16,10 +16,22 @@ SELECT
         event_action AS action,
         event_feature AS feature,
         SUM(event_sampling) AS events
-    FROM Kartographer_16132745
+    FROM Kartographer_16132745_15423246
     WHERE
         timestamp >= '{from_timestamp}'
         AND timestamp < '{to_timestamp}'
     GROUP BY event_action, event_feature
 
+UNION ALL
+
+SELECT
+        DATE(timestamp) AS day,
+        event_action AS action,
+        event_feature AS feature,
+        SUM(event_sampling) AS events
+    FROM Kartographer_16132745
+    WHERE
+        timestamp >= '{from_timestamp}'
+        AND timestamp < '{to_timestamp}'
+    GROUP BY event_action, event_feature
 ;
